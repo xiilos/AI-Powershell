fe2a1cb50499361e099f6cc23525e0f8b3faf1b4
Xiilos <kouretas.k@gmail.com>
2018-10-23 12:37:53 -0500

    Added Change to Regedit

--- GP_Results.ps1
+++ GP_Results.ps1
@@ -6,10 +6,23 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 # Group Policy Results
-
+do {
 Write-Host "Getting Group Policy Results"
+gpupdate
 gpresult /r >c:\Group_policy_Report.txt
 Invoke-Item "c:\Group_policy_Report.txt"
+
+Write-Host "If you get an error stating (No User Data in RSOP) you may have to edit the GPO History in REGEDIT"
+$confirmation = Read-Host "Would you like me to remove and update the GPO History for you? [Y/N]"
+if ($confirmation -eq 'y') {
+  Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Group Policy\History' -Name DCName | out-null
+  Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Group Policy\History' -Name PolicyOverdue | out-null
+}
+
+
+$repeat = Read-Host 'Do you want to run it again? [Y/N]'
+
+} Until ($repeat -eq 'n')
 
 Write-Output "Quitting"
 Get-PSSession | Remove-PSSession
