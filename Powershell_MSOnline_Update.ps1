9857a39bb2bba817710f005dd5e77cc67bb4040c
Xiilos <kouretas.k@gmail.com>
2018-10-24 10:20:42 -0500

    Updated code to retrieve powershell download

--- Powershell_MSOnline_Update.ps1
+++ Powershell_MSOnline_Update.ps1
@@ -6,7 +6,34 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 # Execution Policy
 
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+Write-Host "Chekcing version of powershell"
+$PSVersionTable.PSVersion
+
+$confirmation = Read-Host "Would you like me to update powershell? [Y/N]"
+if ($confirmation -eq 'y') {
+  
+
+Write-Host "Updating Powershell"
+$url = "https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/W2K12-KB3191565-x64.msu"
+$output = "$PSScriptRoot\Powershell_Update"
+$start_time = Get-Date
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+#OR
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+}
+
+
+$confirmation = Read-Host "Would you like me to Install MSonline Module? [Y/N]"
+if ($confirmation -eq 'y') {
+Write-Host "Adding Azure MSonline module"
+Install-Module MSonline -Confirm:$false
+
+}
 
 
 
