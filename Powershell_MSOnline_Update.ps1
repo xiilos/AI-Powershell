7beefe24e8145d18f1ed4ac7e75910b8984591c6
Xiilos <kouretas.k@gmail.com>
2018-10-24 09:58:41 -0500

    New

---
+++ Powershell_MSOnline_Update.ps1
@@ -0,0 +1,17 @@
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+# Execution Policy
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+
+
+Write-Output "Quitting"
+Get-PSSession | Remove-PSSession
+Exit
+
+# End Scripting
