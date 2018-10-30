41a78885e561e47a870b07c76a34d09e2dcd18d7
Xiilos <kouretas.k@gmail.com>
2018-10-30 11:36:16 -0500

    new

--- Restart Service.ps1
+++ Restart Service.ps1
@@ -10,10 +10,7 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 #Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 
 
-Write-Host "Updating Add2Exchange"
-Write-Host "Downloading the latest Add2Exchange"
-
-Invoke-WebRequest "ftp://ftp.diditbetter.com/A2E-Enterprise/A2EAutoUpdate/Add2Exchange_Upgrade.msi" -outfile "c:\zlibrary\Add2Exchange_Upgrade.msi"
+Restart-Service -SERVERNAME 3cx-phone -SERVICENAME 3cxmediaserver
 
 Write-Host "Done"
 Write-Host "Quitting"
