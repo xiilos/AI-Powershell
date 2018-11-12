a04f6ac444f71a5efc228b6eacc33ecbb4b28b7b
Xiilos <kouretas.k@gmail.com>
2018-11-12 12:46:11 -0600

    Added Sleep and restart

--- Restart_Service.ps1
+++ Restart_Service.ps1
@@ -40,6 +40,40 @@ if ($WaitForIt -ne "") {
     Write-host "$Result`: $SvcName on $SvrName is $($svc.status)"
 }
 
+Write-Host "Waiting for Service"
+Start-sleep -10
+
+[string]$WaitForIt = ""
+[string]$Verb = ""
+[string]$Result = "FAILED"
+$svc = (get-service -computername $SvrName -name $SvcName)
+Write-host "$SvcName on $SvrName is $($svc.status)"
+Switch ($svc.status) {
+    'Stopped' {
+        Write-host "Starting $SvcName..."
+        $Verb = "start"
+        $WaitForIt = 'Running'
+        $svc.Start()}
+    'Running' {
+        Write-host "Restarting $SvcName..."
+        $Verb = "Restart"
+        $WaitForIt = 'Restarting'
+        $svc.stop()}
+    Default {
+        Write-host "$SvcName is $($svc.status).  Taking no action."}
+}
+if ($WaitForIt -ne "") {
+    Try {  
+        $svc.WaitForStatus($WaitForIt,'00:02:00')
+    } Catch {
+        Write-host "After waiting for 2 minutes, $SvcName failed to $Verb."
+    }
+    $svc = (get-service -computername $SvrName -name $SvcName)
+    if ($svc.status -eq $WaitForIt) {$Result = 'SUCCESS'}
+    Write-host "$Result`: $SvcName on $SvrName is $($svc.status)"
+}
+
+
 Write-Host "Done"
 Write-Host "Quitting"
 Get-PSSession | Remove-PSSession
