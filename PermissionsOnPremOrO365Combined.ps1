ca57989055b5b90db029d6febdaeffdec5256c7b
xiilos <kouretas.k@gmail.com>
2018-12-05 11:41:05 -0600

    Added Exchange 2010

--- PermissionsOnPremOrO365Combined.ps1
+++ PermissionsOnPremOrO365Combined.ps1
@@ -11,14 +11,15 @@ $question = 'Pick one of the following from below'
 
 $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
 $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Office 365'))
-$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Exchange2013-2016'))
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Exchange 2013-2016'))
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&MExchange 2010'))
 $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Quit'))
 
-$decision = $Host.UI.PromptForChoice($message, $question, $choices, 2)
+$decision = $Host.UI.PromptForChoice($message, $question, $choices, 3)
 
-# Option 2: Quit
+# Option 3: Quit
 
-if ($decision -eq 2) {
+if ($decision -eq 3) {
 
 Write-Host "Quitting"
 Get-PSSession | Remove-PSSession
@@ -395,6 +396,200 @@ if ($decision -eq 7) {
   Get-PSSession | Remove-PSSession
   Exit
 }
+}
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Option 2: Exchange 2010
+
+
+if ($decision -eq 2) {
+
+
+  add-pssnapin Microsoft.Exchange.Management.PowerShell.E2010;
+  Set-ADServerSettings -ViewEntireForest $true
+  
+  Write-Host "The next prompt will ask for the Sync Service Account name in the format Example: zAdd2Exchange or zAdd2Exchange@yourdomain.com"
+  $User = read-host "Enter Sync Service Account";
+  
+  $message  = 'Do you Want to remove or Add Add2Exchange Permissions'
+  $question = 'Pick one of the following from below'
+  
+  $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&0 Add Exchange Perm'))
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&1 Remove Exchange Perm'))
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&2 Both-Remove/Add'))
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&3 Add Dist-List Perm'))
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&4 Remove Dist-List Perm'))
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&5 Add Single Perm'))
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&6 Remove Single Perm'))
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&7 Quit'))
+  
+  
+  
+  $decision = $Host.UI.PromptForChoice($message, $question, $choices, 7)
+  
+  # Option 0: Exchange 2010 on Premise-Adding new permissions all
+  
+  if ($decision -eq 0) {
+  Write-Host "Adding Permissions to Users"
+  Get-Mailbox -Resultsize Unlimited | Add-MailboxPermission -User $User -AccessRights 'FullAccess' -InheritanceType all -AutoMapping:$false -Confirm:$false
+  Write-Host "Adding Throttling Policy"
+  New-ThrottlingPolicy A2EPolicy -RCAMaxConcurrency $null -RCAPercentTimeInAD $null -RCAPercentTimeInCAS $null -RCAPercentTimeInMailboxRPC $null -EWSMaxConcurrency $null -EWSPercentTimeInAD $null -EWSPercentTimeInCAS $null -EWSPercentTimeInMailboxRPC $null -EWSMaxSubscriptions $null -EWSFastSearchTimeoutInSeconds $null -EWSFindCountLimit $null
+  Set-Mailbox $User -ThrottlingPolicy A2EPolicy
+  Write-Host "Checking............"
+  Start-Sleep -s 2
+  Write-Host "All Done"
+  Write-Host "Quitting"
+  Get-PSSession | Remove-PSSession
+  Exit
+  } 
+  
+  # Option 1: Exchange 2010 on Premise-Remove old Add2Exchange permissions
+  
+  if ($decision -eq 1) {
+  Write-Host "Removing Old zAdd2Exchange Permissions"
+  Remove-ADPermission -Identity "Exchange Administrative Group (FYDIBOHF23SPDLT)" -User $User -AccessRights ExtendedRight -ExtendedRights "View information store status" -InheritanceType Descendents -Confirm:$false
+  Get-MailboxDatabase | Remove-ADPermission -User $User -AccessRights GenericAll -Confirm:$false
+  Get-Mailbox -Resultsize Unlimited | Remove-mailboxpermission -user $User -accessrights FullAccess -verbose -Confirm:$false
+  Write-Host "Checking.............................."
+  Get-MailboxDatabase | Remove-ADPermission -User $User -AccessRights ExtendedRight -ExtendedRights Send-As, Receive-As, ms-Exch-Store-Admin -Confirm:$false
+  Write-Host "Done"
+  Write-Host "Quitting"
+  Get-PSSession | Remove-PSSession
+  Exit
+  }
+  
+  # Option 2: Exchange 2010 on Premise-Remove/Add Permissions all
+  
+  if ($decision -eq 2) {
+  Write-Host "Removing Old zAdd2Exchange Permissions"
+  Remove-ADPermission -Identity "Exchange Administrative Group (FYDIBOHF23SPDLT)" -User $User -AccessRights ExtendedRight -ExtendedRights "View information store status" -InheritanceType Descendents -Confirm:$false
+  Get-MailboxDatabase | Remove-ADPermission -User $User -AccessRights GenericAll -Confirm:$false
+  Get-Mailbox -Resultsize Unlimited | Remove-mailboxpermission -user $User -accessrights FullAccess -Verbose -Confirm:$false
+  Write-Host "Checking.............................."
+  Get-MailboxDatabase | Remove-ADPermission -User $User -AccessRights ExtendedRight -ExtendedRights Send-As, Receive-As, ms-Exch-Store-Admin -Confirm:$false
+  Write-Host "Success....."
+  Write-Host "Adding Permissions to Users"
+  Get-Mailbox -Resultsize Unlimited | Add-MailboxPermission -User $User -AccessRights 'FullAccess' -InheritanceType all -AutoMapping:$false -Confirm:$false
+  Write-Host "Checking............"
+  Write-Host "Adding Throttling Policy"
+  New-ThrottlingPolicy A2EPolicy -RCAMaxConcurrency $null -RCAPercentTimeInAD $null -RCAPercentTimeInCAS $null -RCAPercentTimeInMailboxRPC $null -EWSMaxConcurrency $null -EWSPercentTimeInAD $null -EWSPercentTimeInCAS $null -EWSPercentTimeInMailboxRPC $null -EWSMaxSubscriptions $null -EWSFastSearchTimeoutInSeconds $null -EWSFindCountLimit $null
+  Set-Mailbox $User -ThrottlingPolicy A2EPolicy
+  Write-Host "All Done"
+  Write-Host "Quitting"
+  Get-PSSession | Remove-PSSession
+  Exit
+  }
+  
+  # Option 3: Exchange 2010 on Premise-Adding Permissions to dist. list
+  
+  if ($decision -eq 3) {
+  
+  do {
+  
+  $DistributionGroupName = read-host "Enter distribution list name (Display Name)";
+  
+  Write-Host "Adding Add2Exchange Permissions"
+  
+  $DistributionGroupName = Get-DistributionGroupMember $DistributionGroupName
+  ForEach ($Member in $DistributionGroupName)
+  {
+  Add-MailboxPermission -Identity $Member.name -User $User -AccessRights 'FullAccess' -InheritanceType all -AutoMapping:$false
+  }
+  $confirmation = Read-Host "Would you like me add the A2E Throttling Policy? [Y/N]"
+  if ($confirmation -eq 'y') {
+  Write-Host "Adding Throttling Policy"
+  New-ThrottlingPolicy A2EPolicy -RCAMaxConcurrency $null -RCAPercentTimeInAD $null -RCAPercentTimeInCAS $null -RCAPercentTimeInMailboxRPC $null -EWSMaxConcurrency $null -EWSPercentTimeInAD $null -EWSPercentTimeInCAS $null -EWSPercentTimeInMailboxRPC $null -EWSMaxSubscriptions $null -EWSFastSearchTimeoutInSeconds $null -EWSFindCountLimit $null
+  Set-Mailbox $User -ThrottlingPolicy A2EPolicy
+  }
+  
+  $repeat = Read-Host 'Do you want to run it again? [Y/N]'
+  
+  } Until ($repeat -eq 'n')
+  
+  Write-Host "Quitting"
+  Get-PSSession | Remove-PSSession
+  Exit
+  }
+  
+  # Option 4: Exchange 2010 on Premise-Removing dist. list permissions
+  
+  if ($decision -eq 4) {
+  
+  do {
+  
+  $DistributionGroupName = read-host "Enter distribution list name (Display Name)";
+  
+  Write-Host "Removing Add2Exchange Permissions"
+  
+  $DistributionGroupName = Get-DistributionGroupMember $DistributionGroupName
+  ForEach ($Member in $DistributionGroupName)
+  {
+  Remove-mailboxpermission -Identity $Member.name -User $User -AccessRights 'FullAccess' -InheritanceType all -Confirm:$false
+  }
+  
+  $repeat = Read-Host 'Do you want to run it again? [Y/N]'
+  
+  } Until ($repeat -eq 'n')
+  
+  Write-Host "Quitting"
+  Get-PSSession | Remove-PSSession
+  Exit
+  }
+  
+  # Option 5: Exchange 2010 on Premise-Adding permissions to single user
+  
+  if ($decision -eq 5) {
+  
+  do {
+  
+  $Identity = read-host "Enter user Email Address";
+  
+  Write-Host "Adding Add2Exchange Permissions to Single User"
+  Add-MailboxPermission -Identity $identity -User $User -AccessRights 'FullAccess' -InheritanceType all -AutoMapping:$false
+  $confirmation = Read-Host "Would you like me add the A2E Throttling Policy? [Y/N]"
+  if ($confirmation -eq 'y') {
+  Write-Host "Adding Throttling Policy"
+  New-ThrottlingPolicy A2EPolicy -RCAMaxConcurrency $null -RCAPercentTimeInAD $null -RCAPercentTimeInCAS $null -RCAPercentTimeInMailboxRPC $null -EWSMaxConcurrency $null -EWSPercentTimeInAD $null -EWSPercentTimeInCAS $null -EWSPercentTimeInMailboxRPC $null -EWSMaxSubscriptions $null -EWSFastSearchTimeoutInSeconds $null -EWSFindCountLimit $null
+  Set-Mailbox $User -ThrottlingPolicy A2EPolicy
+  }
+  
+  $repeat = Read-Host 'Do you want to run it again? [Y/N]'
+  
+  } Until ($repeat -eq 'n')
+  
+  Write-Host "Quitting"
+  Get-PSSession | Remove-PSSession
+  Exit
+  }
+  
+  # Option 6: Exchange 2010 on Premise-Removing permissions to single user
+  
+  if ($decision -eq 6) {
+  do {
+  
+  $Identity = read-host "Enter user Email Address"
+  
+  Write-Host "Removing Add2Exchange Permissions to Single User"
+  Remove-MailboxPermission -Identity $identity -User $User -AccessRights 'FullAccess' -InheritanceType all -Confirm:$false
+  
+  $repeat = Read-Host 'Do you want to run it again? [Y/N]'
+  
+  } Until ($repeat -eq 'n')
+  
+  Write-Host "Quitting"
+  Get-PSSession | Remove-PSSession
+  Exit
+  }
+  
+  # Option 7: Exchange on Premise- Quit
+  
+  if ($decision -eq 7) {
+    Write-Host "Quitting"
+    Get-PSSession | Remove-PSSession
+    Exit
+  }
 }
 
 
