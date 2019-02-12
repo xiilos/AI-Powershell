80a47c923e1651278ae5d31f710c4a2f4ae17b7b
xiilos <kouretas.k@gmail.com>
2019-02-12 12:18:52 -0600

    Started New Project

---
+++ In Development/Auto_Migration.ps1
@@ -0,0 +1,22 @@
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+#Execution Policy
+
+#Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+
+# Script #
+
+
+
+Write-Host "Done"
+Write-Host "Quitting"
+Get-PSSession | Remove-PSSession
+Exit
+
+# End Scripting
