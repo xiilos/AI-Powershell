045d576b14bb7ffd2c35edbf7003d2d3874634d0
xiilos <kouretas.k@gmail.com>
2019-02-28 22:01:42 -0600

    Formatting

--- Timed Execution Permissions/Permissions_Task_Creation.ps1
+++ Timed Execution Permissions/Permissions_Task_Creation.ps1
@@ -11,16 +11,20 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass
 
 # Script #
 
+#Check for Powershell File Paths
+
+$Location = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\OpenDoor Software®\Add2Exchange' -Name "InstallLocation").InstallLocation
+Set-Location $Location
+
 #Check and Create Stored Credentials
 
 Write-Host "Creating Secure Location"
-$TestPath = "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Creds"
+$TestPath = ".\Setup\Timed Permissions\Creds"
 if ( $(Try { Test-Path $TestPath.trim() } Catch { $false }) ) {
-
 Write-Host "Secure Location Exists...Resuming"
  }
 Else {
-New-Item -ItemType directory -Path "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Creds"
+New-Item -ItemType directory -Path ".\Setup\Timed Permissions\Creds"
  }
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -28,64 +32,101 @@ New-Item -ItemType directory -Path "C:\Program Files (x86)\OpenDoor Software®\A
 #Create Secure Credentials File
 
 Do {
-
+Write-Host "We will now create your Secure Credential Files"
 #Checking Source Tenent or Exchange Admin Username
 
-Write-Host "We will now create your Secure Credentials File"
-
-$TestPath = "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Creds\ServerUser.txt"
+$TestPath = ".\Setup\Timed Permissions\Creds\ServerUser.txt"
 if ( $(Try { Test-Path $TestPath.trim() } Catch { $false }) ) {
-$confirmation = Read-Host "Exchange/Tenent Admin Username File Exists...Create a New One? [Y/N]"
-if ($confirmation -eq 'N') {
-Write-Host "Resuming"
-}
+  Write-Host "Exchange/Tenent Admin Username File Exists...Resuming"
+   }
 
-if ($confirmation -eq 'Y') {
-Read-Host "Type in the Admin Username to Connect to your Exchange Server. Press Enter when Finished." | out-file "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Creds\ServerUser.txt"
-}
-}
+  Else {
+  Read-Host "Type in the Admin Username to Connect to your Exchange Server. Press Enter when Finished." | out-file ".\Setup\Timed Permissions\Creds\ServerUser.txt"
+   }
+   $confirmation = Read-Host "Do it Again? or Create a New One? [Y/N]"
+   if ($confirmation -eq 'N') {
+   Write-Host "Resuming"
+   }
+   
+   if ($confirmation -eq 'Y') {
+   Read-Host "Type in the Admin Username to Connect to your Exchange Server. Press Enter when Finished." | out-file ".\Setup\Timed Permissions\Creds\ServerUser.txt"
+   }
 
 #Checking Source Tenent or Exchange Admin Password
 
-$TestPath = "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Creds\ServerPass.txt"
+$TestPath = ".\Setup\Timed Permissions\Creds\ServerPass.txt"
 if ( $(Try { Test-Path $TestPath.trim() } Catch { $false }) ) {
-$confirmation = Read-Host "Exchange/Tenent Admin Password File Exists...Create a New One? [Y/N]"
-if ($confirmation -eq 'N') {
-Write-Host "Resuming"
-}
+  Write-Host "Exchange/Tenent Admin Password File Exists...Resuming"
+   }
 
-if ($confirmation -eq 'Y') {
-Read-Host "Type in the Admin Password or Tenent Admin Password to Connect to your Exchange Server/Office 365. Press Enter when Finished." | out-file "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Creds\ServerPass.txt"
-}
-}
+  Else {
+  Read-Host "Type in the Admin Password or Tenent Admin Password to Connect to your Exchange Server/Office 365. Press Enter when Finished." -assecurestring | convertfrom-securestring | out-file ".\Setup\Timed Permissions\Creds\ServerPass.txt"
+   }
+   $confirmation = Read-Host "Do it Again? or Create a New One? [Y/N]"
+   if ($confirmation -eq 'N') {
+   Write-Host "Resuming"
+   }
+   
+   if ($confirmation -eq 'Y') {
+   Read-Host "Type in the Admin Password or Tenent Admin Password to Connect to your Exchange Server/Office 365. Press Enter when Finished." | out-file ".\Setup\Timed Permissions\Creds\ServerPass.txt"
+   }
 
 #Checking Source Exchange Name
 
-$TestPath = "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Creds\ExchangeName.txt"
+$TestPath = ".\Setup\Timed Permissions\Creds\ExchangeName.txt"
 if ( $(Try { Test-Path $TestPath.trim() } Catch { $false }) ) {
-$confirmation = Read-Host "Exchange Server Name File Exists...Create a New One? [Y/N]"
-if ($confirmation -eq 'N') {
-Write-Host "Resuming"
-}
+  Write-Host "Exchange Server Name File Exists...Resuming"
+   }
 
-if ($confirmation -eq 'Y') {
-Read-Host "Type in your Exchange Server Name. Example: ExchangeServer01 Press Enter when Finished." | out-file "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Creds\ExchangeName.txt"
-}
-}
+  Else {
+  Read-Host "Type in your Exchange Server Name. Example: ExchangeServer01 Press Enter for none or when Finished." | out-file ".\Setup\Timed Permissions\Creds\ExchangeName.txt"
+   }
+   $confirmation = Read-Host "Do it Again? or Create a New One? [Y/N]"
+   if ($confirmation -eq 'N') {
+   Write-Host "Resuming"
+   }
+   
+   if ($confirmation -eq 'Y') {
+   Read-Host "Type in your Exchange Server Name. Example: ExchangeServer01 Press Enter for none or when Finished." | out-file ".\Setup\Timed Permissions\Creds\ExchangeName.txt"
+   }
+
+#Checking Source Distribution List Name
+
+$TestPath = ".\Setup\Timed Permissions\Creds\DistributionName.txt"
+if ( $(Try { Test-Path $TestPath.trim() } Catch { $false }) ) {
+  Write-Host "Distribution List Name File Exists...Resuming"
+   }
+
+  Else {
+  Read-Host "Type in your Distribution List Name. Example: zFirmCalendarSync or for multiple list: 'zFirmCalendar','ZFirmContacts' <-Exactley as shown. Press Enter for none or when Finished." | out-file ".\Setup\Timed Permissions\Creds\DistributionName.txt"
+   }
+   $confirmation = Read-Host "Do it Again? or Create a New One? [Y/N]"
+   if ($confirmation -eq 'N') {
+   Write-Host "Resuming"
+   }
+   
+   if ($confirmation -eq 'Y') {
+   Read-Host "Type in your Distribution List Name. Example: zFirmCalendarSync or for multiple list: 'zFirmCalendar','ZFirmContacts' <-Exactley as shown. Press Enter for none or when Finished." | out-file ".\Setup\Timed Permissions\Creds\DistributionName.txt"
+   }
 
 #Checking Source Service Account Name
 
-$TestPath = "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Creds\ServiceAccount.txt"
+$TestPath = ".\Setup\Timed Permissions\Creds\ServiceAccount.txt"
 if ( $(Try { Test-Path $TestPath.trim() } Catch { $false }) ) {
-$confirmation = Read-Host "Service Account Name File Exists...Create a New One? [Y/N]"
-if ($confirmation -eq 'N') {
-Write-Host "Resuming"
-}
+  Write-Host "Service Account Name File Exists...Resuming"
+   }
 
-if ($confirmation -eq 'Y') {
-Read-Host "Type in your Sync Service Account Same. Example: zAdd2Exchange Press Enter when Finished." | out-file "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Creds\ServiceAccount.txt"
-}
-}
+  Else {
+  Read-Host "Type in your Sync Service Account Same. Example: zAdd2Exchange Press Enter when Finished." | out-file ".\Setup\Timed Permissions\Creds\ServiceAccount.txt"
+   }
+   $confirmation = Read-Host "Do it Again? or Create a New One? [Y/N]"
+   if ($confirmation -eq 'N') {
+   Write-Host "Resuming"
+   }
+   
+   if ($confirmation -eq 'Y') {
+   Read-Host "Type in your Sync Service Account Same. Example: zAdd2Exchange Press Enter when Finished." | out-file ".\Setup\Timed Permissions\Creds\ServiceAccount.txt"
+   }
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -121,14 +162,14 @@ if ($choice -eq 0) {
 #Office 365 All Permissions
 if ($Decision -eq 0) {
 $Trigger= New-ScheduledTaskTrigger -Daily -At 5am
-$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '-NoProfile -WindowStyle Hidden -Executionpolicy Bypass -file "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Office365_All_Permissions.ps1"'
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -WorkingDirectory $Location -Argument '-NoProfile -WindowStyle Hidden -Executionpolicy Bypass -file .\Setup\Timed Permissions\Office365_All_Permissions.ps1"'
 Register-ScheduledTask -Action $Action -RunLevel Highest -Trigger $Trigger -TaskName "Add2Exchange Permissions" -Description "Adds Add2Exchange Permissions Automatically to users mailboxes"
   }
 
 #Office 365 Dist. List
 if ($Decision -eq 1) {
 $Trigger= New-ScheduledTaskTrigger -Daily -At 5am
-$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '-NoProfile -WindowStyle Hidden -Executionpolicy Bypass -file "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed_Permissions\Office365_Dist_List_Permissions.ps1"'
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -WorkingDirectory $Location -Argument '-NoProfile -WindowStyle Hidden -Executionpolicy Bypass -file ".\Setup\Timed Permissions\Office365_Dist_List_Permissions.ps1"'
 Register-ScheduledTask -Action $Action -RunLevel Highest -Trigger $Trigger -TaskName "Add2Exchange Permissions" -Description "Adds Add2Exchange Permissions Automatically to users mailboxes"
   }
 
@@ -146,10 +187,38 @@ if ($Decision -eq 2) {
 
 if ($choice -eq 1) {
 
+  $message  = 'Please Pick how you want to connect'
+  $question = 'Pick one of the following from below'
+  
+  $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Add Permissions to All Users'))
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Add Permissions to only Distribution Lists'))
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Quit'))
+  
+  $Decision = $Host.UI.PromptForChoice($message, $question, $choices, 2)
+
+#Exchange 2010 All Permissions
+if ($Decision -eq 0) {
 $Trigger= New-ScheduledTaskTrigger -Daily -At 5am
-$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '-NoProfile -WindowStyle Hidden -Executionpolicy Bypass -file "C:\Timed Permissions\2010_All_Timed_Permissions.ps1"'
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -WorkingDirectory $Location -Argument '-NoProfile -WindowStyle Hidden -Executionpolicy Bypass -file ".\Setup\Timed Permissions\2010_All_Permissions.ps1"'
 Register-ScheduledTask -Action $Action -RunLevel Highest -Trigger $Trigger -TaskName "Add2Exchange Permissions" -Description "Adds Add2Exchange Permissions Automatically to users mailboxes"
   }
+
+#Exchange 2010 Dist. List
+if ($Decision -eq 1) {
+$Trigger= New-ScheduledTaskTrigger -Daily -At 5am
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -WorkingDirectory $Location -Argument '-NoProfile -WindowStyle Hidden -Executionpolicy Bypass -file ".\Setup\Timed Permissions\2010_Dist_List_Permissions.ps1"'
+Register-ScheduledTask -Action $Action -RunLevel Highest -Trigger $Trigger -TaskName "Add2Exchange Permissions" -Description "Adds Add2Exchange Permissions Automatically to users mailboxes"
+  }
+
+#Quitting Exchange 2010
+if ($Decision -eq 2) {
+    Write-Host "Quitting"
+    Get-PSSession | Remove-PSSession
+    Exit
+
+  }
+}
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -157,12 +226,40 @@ Register-ScheduledTask -Action $Action -RunLevel Highest -Trigger $Trigger -Task
 
 if ($choice -eq 2) {
 
-  $Path = "C:\Program Files (x86)\OpenDoor Software®\Add2Exchange\Setup\Timed Permissions\2013-2016_All_Timed_Permissions.ps1"
-  $Action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument -ExecutionPolicy Bypass -WindowStyle Hidden $Path
+  $message  = 'Please Pick how you want to connect'
+  $question = 'Pick one of the following from below'
   
-  $Trigger =  New-ScheduledTaskTrigger -Daily -At 5am
-  Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "Add2Exchange Permissions" -Description "Adds Add2Exchange Permissions Automatically to users mailboxes"
+  $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Add Permissions to All Users'))
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Add Permissions to only Distribution Lists'))
+  $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Quit'))
+  
+  $Decision = $Host.UI.PromptForChoice($message, $question, $choices, 2)
+
+#Exchange 2013-2016 All Permissions
+if ($Decision -eq 0) {
+$Trigger= New-ScheduledTaskTrigger -Daily -At 5am
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -WorkingDirectory $Location -Argument '-NoProfile -WindowStyle Hidden -Executionpolicy Bypass -file ".\Setup\Timed Permissions\2013-2016_All_Permissions.ps1"'
+Register-ScheduledTask -Action $Action -RunLevel Highest -Trigger $Trigger -TaskName "Add2Exchange Permissions" -Description "Adds Add2Exchange Permissions Automatically to users mailboxes"
   }
+
+#Exchange 2013-2016 Dist. List
+if ($Decision -eq 1) {
+$Trigger= New-ScheduledTaskTrigger -Daily -At 5am
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -WorkingDirectory $Location -Argument '-NoProfile -WindowStyle Hidden -Executionpolicy Bypass -file ".\Setup\Timed Permissions\2013-2016_Dist_List_Permissions.ps1"'
+Register-ScheduledTask -Action $Action -RunLevel Highest -Trigger $Trigger -TaskName "Add2Exchange Permissions" -Description "Adds Add2Exchange Permissions Automatically to users mailboxes"
+  }
+
+#Quitting Exchange 2013-2016
+if ($Decision -eq 2) {
+    Write-Host "Quitting"
+    Get-PSSession | Remove-PSSession
+    Exit
+
+  }
+}
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 $repeat = Read-Host 'Do you want to run it again? [Y/N]'
 } Until ($repeat -eq 'n')
