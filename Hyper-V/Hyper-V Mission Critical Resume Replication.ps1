b1d94949f38df4ea98a8c4ab9644c98495da2e3d
xiilos <kouretas.k@gmail.com>
2019-03-19 15:13:16 -0500

    Formatting

--- Hyper-V/Hyper-V Mission Critical Resume Replication.ps1
+++ Hyper-V/Hyper-V Mission Critical Resume Replication.ps1
@@ -1,19 +1,20 @@
-if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-{
-  # Relaunch as an elevated process:
-  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
-  exit
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    # Relaunch as an elevated process:
+    Start-Process powershell.exe "-File", ('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+    exit
 }
+
+
+#Execution Policy
+Set-ExecutionPolicy -ExecutionPolicy Bypass
 
 #Notes: Resumes and Resyncs all Missions Critical VMs
 
 # Script #
 
-Resume-VMReplication â€“VMName "3cx-phone","DIBDC","DIBDC1","DIBEX10","Docker-a","IIS","MOJO-B","Redmine","SpamTitan","Spree-B","SQL","Veeam" -Resynchronize
+Resume-VMReplication –VMName "3cx-phone", "DIBDC", "DIBDC1", "DIBEX10", "Docker-a", "IIS", "MOJO-B", "Redmine", "SpamTitan", "Spree-B", "SQL", "Veeam" -Resynchronize
 
 Write-Host "Done"
-Write-Host "Quitting"
+Write-Output "Quitting"
 Get-PSSession | Remove-PSSession
 Exit
-
-# End Scripting
--- Hyper-V/Hyper-V Resync All_V2.ps1
+++ Hyper-V/Hyper-V Resync All_V2.ps1
@@ -1,25 +1,25 @@
-if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-{
-  # Relaunch as an elevated process:
-  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
-  exit
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    # Relaunch as an elevated process:
+    Start-Process powershell.exe "-File", ('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+    exit
 }
+
+#Execution Policy
+Set-ExecutionPolicy -ExecutionPolicy Bypass
 
 # Script #
 
 $Hosts = Read-Host "Hyper-V Host Name?"
-ForEach ($Server in $Hosts)
-{
-$Server
-Invoke-Command -ComputerName $Server {
-$FailedReplicas = Get-VMReplication | Where-Object{$_.Health -EQ 'Critical'}
-ForEach ($VM in $FailedReplicas)
-{
-$VMName = $VM.Name
-$VMName
-Resume-VMReplication $VMName -Resynchronize
-}
-}
+ForEach ($Server in $Hosts) {
+    $Server
+    Invoke-Command -ComputerName $Server {
+        $FailedReplicas = Get-VMReplication | Where-Object {$_.Health -EQ 'Critical'}
+        ForEach ($VM in $FailedReplicas) {
+            $VMName = $VM.Name
+            $VMName
+            Resume-VMReplication $VMName -Resynchronize
+        }
+    }
 }
 
 Write-Host "Done"
