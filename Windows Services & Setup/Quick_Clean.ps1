3ba7a13f0977be15cbe134ae0fb46c5b2c1172b0
xiilos <kouretas.k@gmail.com>
2019-04-05 18:28:48 -0500

    New File

---
+++ Windows Services & Setup/Quick_Clean.ps1
@@ -0,0 +1,36 @@
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+#Execution Policy
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass
+
+
+# Script #
+Write-Host "Flushing DNS"
+IPConfig /FlushDNS
+Write-Host "Done"
+
+Write-Host "Cleaning up Files via Disk Cleanup"
+CleanMgr /verylowdisk -wait
+Write-Host "Done"
+
+Write-Host "Running CCleaner for Temp Files"
+Push-Location ./ccPortable
+Start-Process ccPortable.exe /AUTO -wait
+Write-Host "Done"
+
+Write-Host "Running CCleaner for the Registry"
+Start-Process ccPortable.exe /Registry -wait
+Write-Host "Done"
+
+Write-Host "Done"
+Write-Host "Quitting"
+Get-PSSession | Remove-PSSession
+Exit
+
+# End Scripting
