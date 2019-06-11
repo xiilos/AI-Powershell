4792c0a4057af8d8176f7a654523188dc147bf37
xiilos <kouretas.k@gmail.com>
2019-06-11 16:04:27 -0500

    Set for Manual Exchange Shell

---
+++ Exchange_Tools&Permissions/Shell.ps1
@@ -0,0 +1,16 @@
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+#Execution Policy
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass
+
+
+# Script #
+Powershell.exe -noexit ".\Shell_Into_Exchange.ps1" -noprofile
+
+# End Scripting
