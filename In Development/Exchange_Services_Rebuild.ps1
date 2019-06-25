7cf950b4975c5fdd297d8ae0b8068ee71ab1c72f
xiilos <kouretas.k@gmail.com>
2019-06-25 12:13:44 -0500

    formatting

--- In Development/Exchange_Services_Rebuild.ps1
+++ In Development/Exchange_Services_Rebuild.ps1
@@ -10,13 +10,27 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass
 
 
 # Script #
-Get-Service | Where-Object { $_.DisplayName –like “Microsoft Exchange *” } | Set-Service –StartupType Automatic
+Get-Service | Where-Object { $_.DisplayName –like "Microsoft Exchange *" } | Set-Service –StartupType Automatic
 
-Get-Service | Where-Object { $_.DisplayName –eq “IIS Admin Service” } | Set-Service –StartupType Automatic
+Get-Service | Where-Object { $_.DisplayName –like "Windows Management Instrumentation" } | Set-Service –StartupType Automatic
 
-Get-Service | Where-Object { $_.DisplayName –eq “IIS Admin Service” } | Start-Service
+Get-Service | Where-Object { $_.DisplayName –like "World Wide Web Publishing Service" } | Set-Service –StartupType Automatic
 
-Get-Service | Where-Object { $_.DisplayName –like “Microsoft Exchange *” } | Start-Service
+Get-Service | Where-Object { $_.DisplayName –like "Tracing Service for Search in Exchange" } | Set-Service –StartupType Automatic
+
+Get-Service | Where-Object { $_.DisplayName –like "Remote Registry" } | Set-Service –StartupType Automatic
+
+Get-Service | Where-Object { $_.DisplayName –like "Performance Logs & Alerts" } | Set-Service –StartupType Automatic
+
+Get-Service | Where-Object { $_.DisplayName –like "Application Identity" } | Set-Service –StartupType Automatic
+
+Get-Service | Where-Object { $_.DisplayName –like "Microsoft Filtering Management Service" } | Set-Service –StartupType Automatic
+
+Get-Service | Where-Object { $_.DisplayName –eq "IIS Admin Service" } | Set-Service –StartupType Automatic
+
+Get-Service | Where-Object { $_.DisplayName –eq "IIS Admin Service" } | Start-Service
+
+Get-Service | Where-Object { $_.DisplayName –like "Microsoft Exchange *" } | Start-Service
 
 
 Write-Host "ttyl"
