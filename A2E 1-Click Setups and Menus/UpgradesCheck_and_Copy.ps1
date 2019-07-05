0a05a84e95034bd4b1c97d38adcc86e77ef4fd07
xiilos <kouretas.k@gmail.com>
2019-07-05 16:54:15 -0500

    Added bread crumbs for past upgrades

--- A2E 1-Click Setups and Menus/UpgradesCheck_and_Copy.ps1
+++ A2E 1-Click Setups and Menus/UpgradesCheck_and_Copy.ps1
@@ -24,12 +24,14 @@ Write-Host "Checking Upgrades Folder for Existing Installs"
 $TestPath = "\\diditbetter\DFS\FTP\download\A2E-Enterprise\Upgrades\$Latest"
 if ( $(Try { Test-Path $TestPath.trim() } Catch { $false }) ) {
 
-    Write-Host "Latest Add2Exchange Upgrade Already Exists in Upgrades Folder"
+    Write-Host "Latest Add2Exchange Upgrade Already Exists in Upgrades Folder" -ForegroundColor Green
 }
 Else {
     Write-Host "Copying Files Needed..."
     Copy-Item "\\diditbetter\DFS\FTP\download\A2E-Enterprise\$Latest" -Destination "\\diditbetter\DFS\FTP\download\A2E-Enterprise\Upgrades"
+    Remove-Item -path "\\diditbetter\DFS\FTP\download\A2E-Enterprise\Upgrades\a2e-enterprise_upgrade.exe" -ErrorAction SilentlyContinue
     Rename-Item -Path "\\diditbetter\DFS\FTP\download\A2E-Enterprise\Upgrades\$Latest" -NewName "a2e-enterprise_upgrade.exe" -ErrorAction SilentlyContinue
+    Copy-Item "\\diditbetter\DFS\FTP\download\A2E-Enterprise\$Latest" -Destination "\\diditbetter\DFS\FTP\download\A2E-Enterprise\Upgrades"
     Write-Host "Done"
 }
 
@@ -41,18 +43,20 @@ $Filter = "a2e-enterprise.*"
 $Latest = Get-ChildItem -Path $Dir -Filter $Filter | Sort-Object LastAccessTime -Descending | Select-Object -First 1
 $Latest.Name
 
-#Testing to See if File Already Exists in Upgrades
+#Testing to See if File Already Exists in Installs
 
 Write-Host "Checking New Installs Folder for Existing Installs"
 $TestPath = "\\diditbetter\DFS\FTP\download\A2E-Enterprise\New Installs\$Latest"
 if ( $(Try { Test-Path $TestPath.trim() } Catch { $false }) ) {
 
-    Write-Host "Latest Add2Exchange Enterprise Full Already Exists in New Installs Folder"
+    Write-Host "Latest Add2Exchange Enterprise Full Already Exists in New Installs Folder" -ForegroundColor Green
 }
 Else {
     Write-Host "Copying Files Needed..."
     Copy-Item "\\diditbetter\DFS\FTP\download\A2E-Enterprise\$Latest" -Destination "\\diditbetter\DFS\FTP\download\A2E-Enterprise\New Installs"
+    Remove-Item -path "\\diditbetter\DFS\FTP\download\A2E-Enterprise\New Installs\a2e-enterprise.exe" -ErrorAction SilentlyContinue
     Rename-Item -Path "\\diditbetter\DFS\FTP\download\A2E-Enterprise\New Installs\$Latest" -NewName "a2e-enterprise.exe" -ErrorAction SilentlyContinue
+    Copy-Item "\\diditbetter\DFS\FTP\download\A2E-Enterprise\$Latest" -Destination "\\diditbetter\DFS\FTP\download\A2E-Enterprise\New Installs"
     Write-Host "Done"
 }
 
@@ -71,12 +75,14 @@ Write-Host "Checking Upgrades Folder for Existing Installs"
 $TestPath = "\\diditbetter\DFS\FTP\download\Add2Outlook\Upgrades\$Latest"
 if ( $(Try { Test-Path $TestPath.trim() } Catch { $false }) ) {
 
-    Write-Host "Latest Add2Outlook Upgrade Already Exists in Upgrades Folder"
+    Write-Host "Latest Add2Outlook Upgrade Already Exists in Upgrades Folder" -ForegroundColor Green
 }
 Else {
     Write-Host "Copying Files Needed..."
     Copy-Item "\\diditbetter\DFS\FTP\download\Add2Outlook\$Latest" -Destination "\\diditbetter\DFS\FTP\download\Add2Outlook\Upgrades"
+    Remove-Item -path "\\diditbetter\DFS\FTP\download\Add2Outlook\Upgrades\Add2Outlook Full Installation.EXE" -ErrorAction SilentlyContinue
     Rename-Item -Path "\\diditbetter\DFS\FTP\download\Add2Outlook\Upgrades\$Latest" -NewName "Add2Outlook Full Installation.EXE" -ErrorAction SilentlyContinue
+    Copy-Item "\\diditbetter\DFS\FTP\download\Add2Outlook\$Latest" -Destination "\\diditbetter\DFS\FTP\download\Add2Outlook\Upgrades"
     Write-Host "Done"
 }
 
@@ -95,16 +101,18 @@ Write-Host "Checking Upgrades Folder for Existing Installs"
 $TestPath = "\\diditbetter\DFS\FTP\download\RMM-Enterprise\Upgrades\$Latest"
 if ( $(Try { Test-Path $TestPath.trim() } Catch { $false }) ) {
 
-    Write-Host "Latest RMM Upgrade Already Exists in Upgrades Folder"
+    Write-Host "Latest RMM Upgrade Already Exists in Upgrades Folder" -ForegroundColor Green
 }
 Else {
     Write-Host "Copying Files Needed..."
     Copy-Item "\\diditbetter\DFS\FTP\download\RMM-Enterprise\$Latest" -Destination "\\diditbetter\DFS\FTP\download\RMM-Enterprise\Upgrades"
+    Remove-Item -path "\\diditbetter\DFS\FTP\download\RMM-Enterprise\Upgrades\rmm-enterprise.exe" -ErrorAction SilentlyContinue
     Rename-Item -Path "\\diditbetter\DFS\FTP\download\RMM-Enterprise\Upgrades\$Latest" -NewName "rmm-enterprise.exe" -ErrorAction SilentlyContinue
+    Copy-Item "\\diditbetter\DFS\FTP\download\RMM-Enterprise\$Latest" -Destination "\\diditbetter\DFS\FTP\download\RMM-Enterprise\Upgrades"
     Write-Host "Done"
 }
 
-Write-Host "All Upgrades Have Been Copied Over!!!"
+Write-Host "All Upgrades Have Been Copied Over!!!" -ForegroundColor Green
 
 Pause
 Write-Host "ttyl"
