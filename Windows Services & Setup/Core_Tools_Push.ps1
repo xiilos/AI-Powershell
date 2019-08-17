9df7df78530f9c027959a66a84bbe11de91e754a
xiilos <kouretas.k@gmail.com>
2019-08-17 15:05:11 -0500

    removed update for mbam

---
+++ Windows Services & Setup/Core_Tools_Push.ps1
@@ -0,0 +1,31 @@
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+#Execution Policy
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass
+
+
+# Script #
+
+#Start-Process "https://downloads.malwarebytes.com/file/mb3" -Wait
+
+Copy-Item "\\fileserv-db1\Sandbox\Port Apps\PortableApps\ccPortable\*" -Destination "\\fileserv-db1\Sandbox\Port Apps\Documents\Core Tools\ccPortable" -Force
+Copy-Item "\\fileserv-db1\Work\Advantage International\PowerShell Tools REPO\AI-Powershell\Windows Services & Setup\Quick_Clean.ps1" -Destination "\\fileserv-db1\Sandbox\Port Apps\Documents\Core Tools" -Force
+Copy-Item "\\fileserv-db1\Work\Advantage International\PowerShell Tools REPO\AI-Powershell\Windows Services & Setup\Server 2016-2019 New Setup.ps1" -Destination "\\fileserv-db1\Sandbox\Port Apps\Documents\Core Tools" -Force
+Copy-Item "\\fileserv-db1\Work\Advantage International\PowerShell Tools REPO\AI-Powershell\Windows Services & Setup\VM_QuickUpdate_Clean.ps1" -Destination "\\fileserv-db1\Sandbox\Port Apps\Documents\Core Tools" -Force
+Copy-Item "\\fileserv-db1\Work\Advantage International\PowerShell Tools REPO\AI-Powershell\Windows Services & Setup\Windows 10 App Removal.ps1" -Destination "\\fileserv-db1\Sandbox\Port Apps\Documents\Core Tools" -Force
+Copy-Item "\\fileserv-db1\Work\Advantage International\PowerShell Tools REPO\AI-Powershell\Windows Services & Setup\Windows 10 New Setup.ps1" -Destination "\\fileserv-db1\Sandbox\Port Apps\Documents\Core Tools" -Force
+
+
+Write-Host "Success"
+Pause
+Write-Host "ttyl"
+Get-PSSession | Remove-PSSession
+Exit
+
+# End Scripting
