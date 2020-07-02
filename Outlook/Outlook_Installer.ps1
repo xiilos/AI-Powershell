c1b675d1bf26ba125d32ffc1ff7eeec25577f21f
xiilos <kouretas.k@gmail.com>
2020-07-02 16:17:46 -0500

    updated and formatting

--- Outlook/Outlook_Installer.ps1
+++ Outlook/Outlook_Installer.ps1
@@ -7,7 +7,7 @@ Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $Outlook365Installer             = New-Object system.Windows.Forms.Form
-$Outlook365Installer.ClientSize  = '279,329'
+$Outlook365Installer.ClientSize  = New-Object System.Drawing.Point(247,169)
 $Outlook365Installer.text        = "Outlook 365 Install"
 $Outlook365Installer.TopMost     = $false
 
@@ -17,51 +17,26 @@ $ProRetail.AutoSize              = $true
 $ProRetail.width                 = 25
 $ProRetail.height                = 10
 $ProRetail.location              = New-Object System.Drawing.Point(21,30)
-$ProRetail.Font                  = 'Microsoft Sans Serif,14,style=Bold,Underline'
-
-$ProBusiness                     = New-Object system.Windows.Forms.Label
-$ProBusiness.text                = "Office 365 Business Retail"
-$ProBusiness.AutoSize            = $true
-$ProBusiness.width               = 25
-$ProBusiness.height              = 10
-$ProBusiness.location            = New-Object System.Drawing.Point(20,170)
-$ProBusiness.Font                = 'Microsoft Sans Serif,14,style=Bold,Underline'
+$ProRetail.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',14,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold -bor [System.Drawing.FontStyle]::Underline))
 
 $Pro32                           = New-Object system.Windows.Forms.Button
 $Pro32.text                      = "O365 Pro x86"
 $Pro32.width                     = 200
 $Pro32.height                    = 30
 $Pro32.location                  = New-Object System.Drawing.Point(20,60)
-$Pro32.Font                      = 'Microsoft Sans Serif,12'
+$Pro32.Font                      = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
 $Pro64                           = New-Object system.Windows.Forms.Button
 $Pro64.text                      = "O365 Pro x64"
 $Pro64.width                     = 200
 $Pro64.height                    = 30
 $Pro64.location                  = New-Object System.Drawing.Point(20,100)
-$Pro64.Font                      = 'Microsoft Sans Serif,12'
+$Pro64.Font                      = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
-$Business32                      = New-Object system.Windows.Forms.Button
-$Business32.text                 = "O365 Business x86"
-$Business32.width                = 200
-$Business32.height               = 30
-$Business32.location             = New-Object System.Drawing.Point(20,200)
-$Business32.Font                 = 'Microsoft Sans Serif,12'
-
-$Business64                      = New-Object system.Windows.Forms.Button
-$Business64.text                 = "O365 Business x64"
-$Business64.width                = 200
-$Business64.height               = 30
-$Business64.location             = New-Object System.Drawing.Point(20,240)
-$Business64.Font                 = 'Microsoft Sans Serif,12'
-
-$Outlook365Installer.controls.AddRange(@($ProRetail,$ProBusiness,$Pro32,$Pro64,$Business32,$Business64))
+$Outlook365Installer.controls.AddRange(@($ProRetail,$Pro32,$Pro64))
 
 $Pro32.Add_Click({Start-Process cmd -Argumentlist "/c Pro_Retailx86.cmd" -WorkingDirectory ".\Setup Files"})
 $Pro64.Add_Click({Start-Process cmd -Argumentlist "/c Pro_Retailx64.cmd" -WorkingDirectory ".\Setup Files"})
-$Business64.Add_Click({Start-Process cmd -Argumentlist "/c Busimess_Retailx64.cmd" -WorkingDirectory ".\Setup Files"})
-$Business32.Add_Click({Start-Process cmd -Argumentlist "/c Busimess_Retailx86.cmd" -WorkingDirectory ".\Setup Files"})
-
 
 [void]$Outlook365Installer.ShowDialog()
 
