2252810f0b382df59ea916a8cd1fb500bbbb285d
xiilos <kouretas.k@gmail.com>
2020-07-16 13:36:55 -0500

    New

--- Outlook/Outlook_Profile_Set.ps1
+++ Outlook/Outlook_Profile_Set.ps1
@@ -12,6 +12,18 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
 
 # Script #
 
+#Check Outlook Version
+$Version = Get-ItemProperty "Registry::HKEY_CLASSES_ROOT\Outlook.Application\CurVer" | Select-object -expand '(default)' -ErrorAction SilentlyContinue -ErrorVariable E1
+If ($E1) {
+  Write-Host "There seems to be an error or conflict in Outlook Versions. Exiting"
+  Pause
+  Exit
+}
+
+#######Outlook V.16#############
+
+If ($Version -eq "Outlook.Application.16") {
+#Profile Check
 Write-Host "Checking Profile Names"
 $Profile = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\WOW6432Node\OpenDoor Software®\Add2Exchange" -Name ServiceAccount
 
@@ -27,12 +39,43 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\$P
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\$Profile\0a0d020000000000c000000000000046" -Name "00033d1b" -value ([byte[]](0x00,0x00,0x00,0x00))
 
 #Trust Center
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Privacy\SettingsStore\Anonymous" -Name "ControllerConnectedServicesState" -value 2
+$Privacy = Test-Path "HKCU:\Software\Microsoft\Office\16.0\Common\Privacy"
+If ($Privacy -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\16.0\Common" -Name "Privacy"
+}
 
-$Date = Get-Date -Format 'yyy\-MM\-ddTHH:mm:ssZ'
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Privacy\SettingsStore\Anonymous" -Name "ControllerConnectedServicesStateTime" -value $Date
+$Anonymous = Test-Path -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Privacy\SettingsStore\Anonymous"
 
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\$Profile\c02ebc5353d9cd11975200aa004ae40e" -Name "00030354" -value ([byte[]](0x00,0x00,0x00,0x00))
+If ($Anonymous -eq $true){
+  Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Privacy\SettingsStore\Anonymous" -Name "ControllerConnectedServicesState" -value 2
+  $Date = Get-Date -Format 'yyy\-MM\-ddTHH:mm:ssZ'
+  Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Privacy\SettingsStore\Anonymous" -Name "ControllerConnectedServicesStateTime" -value $Date
+}
+
+If ($Anonymous -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Privacy" -Name "SettingsStore"
+  New-Item -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Privacy\SettingsStore" -Name "Anonymous"
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Privacy\SettingsStore\Anonymous" -Name "ControllerConnectedServicesState" -value 2
+  $Date = Get-Date -Format 'yyy\-MM\-ddTHH:mm:ssZ'
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Privacy\SettingsStore\Anonymous" -Name "ControllerConnectedServicesStateTime" -value $Date
+}
+
+
+$Profile1 = Test-Path -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\$Profile\c02ebc5353d9cd11975200aa004ae40e"
+
+If ($Profile1 -eq $true){
+  Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\$Profile\c02ebc5353d9cd11975200aa004ae40e" -Name "00030354" -value ([byte[]](0x00,0x00,0x00,0x00))
+}
+
+If ($Profile1 -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\$Profile" -Name "c02ebc5353d9cd11975200aa004ae40e"
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Profiles\$Profile\c02ebc5353d9cd11975200aa004ae40e" -Name "00030354" -value ([byte[]](0x00,0x00,0x00,0x00))
+}
+
+$Security = Test-Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Security"
+If ($Security -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook" -Name "Security"
+}
 
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Security" -Name "InitEncrypt" -value 2
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Security" -Name "InitSign" -value 2
@@ -41,27 +84,204 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Security" -
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Security" -Name "Level" -value 1
 
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Preferences" -Name "DisableAttachmentPreviewing" -value 1
+
+$Mail = Test-Path -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail"
+If ($Mail -eq $true) {
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Name "UnblockSafeZone" -value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Name "UnblockSpecificSenders" -value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Name "UnblockRSS" -value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Name "UnblockSTS" -value 0
+}
+
+
+If ($Mail -eq $false) {
+  New-Item -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options" -Name "Mail"
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Name "UnblockSafeZone" -value 0
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Name "UnblockSpecificSenders" -value 0
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Name "UnblockRSS" -value 0
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Name "UnblockSTS" -value 0
+  }
 
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common" -Name "SendCustomerDataOptInReason" -value 1
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common" -Name "SendCustomerDataOptIn" -value 1
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common" -Name "SendCustomerData" -value 1
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common" -Name "UpdateReliabilityData" -value 0
 
+$Helperviewer = Test-Path "HKCU:\Software\Microsoft\Office\16.0\Common\HelpViewer"
+If ($Helperviewer -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\16.0\Common" -Name "HelpViewer"
+}
+
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common\HelpViewer" -Name "UseOnlineContent" -value 2
+
+$PTWatson = Test-Path "HKCU:\Software\Microsoft\Office\16.0\Common\PTWatson"
+If ($PTWatson -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\16.0\Common" -Name "PTWatson"
+}
+
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common\PTWatson" -Name "PTWOptIn" -value 3
+
+$ResearchOptions = Test-Path "HKCU:\Software\Microsoft\Office\16.0\Common\Research\Options"
+If ($ResearchOptions -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Research" -Name "Options"
+}
+
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Research\Options" -Name "DiscoveryNeedOptIn" -value 0
 
-Set-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Wow6432Node\Microsoft\Office\16.0\Outlook\Security" -Name "ObjectModelGuard" -value 2
+$Security = Test-Path -Path "HKLM:SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Wow6432Node\Microsoft\Office\16.0\Outlook\Security"
+
+If ($Security -eq $true){
+  Set-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Wow6432Node\Microsoft\Office\16.0\Outlook\Security" -Name "ObjectModelGuard" -value 2
+}
+
+If ($Security -eq $false){
+  New-Item -Path "HKLM:SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Wow6432Node\Microsoft\Office\16.0\Outlook" -Name "Security"
+  New-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Wow6432Node\Microsoft\Office\16.0\Outlook\Security" -Name "ObjectModelGuard" -value 2
+}
+
 
 #Outlook Social Connector
+Write-Host "Disabling Outlook Social Connector"
 Start-Process Powershell .\OSC_Disable.bat
 
 Write-Host "Done"
 Pause
+} 
+
+#######Outlook V.15#############
+
+If ($Version -eq "Outlook.Application.15") {
+  #Profile Check
+Write-Host "Checking Profile Names"
+$Profile = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\WOW6432Node\OpenDoor Software®\Add2Exchange" -Name ServiceAccount
+
+Write-Host "Add2Exchange Profile in use is $Profile"
+
+Write-Host "Writing $Profile Profile Changes...."
+
+#Address Book
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Profiles\$Profile\9207f3e0a3b11019908b08002b2a56c2" -Name "01023d06" -value ([byte[]](0x00,0x00,0x00,0x00,0xb1,0x84,0xb4,0xea,0xd0,0xab,0xcb,0x41,0xa8,0xd0,0x0d,0xea,0x0c,0x29,0xb0,0x44,0x01,
+0x00,0x00,0x00,0x00,0x01,0x00,0x00,0x2f,0x00))
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Profiles\$Profile\0a0d020000000000c000000000000046" -Name "000b3d1c" -value ([byte[]](0x00,0x00))
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Profiles\$Profile\0a0d020000000000c000000000000046" -Name "00033d1b" -value ([byte[]](0x00,0x00,0x00,0x00))
+
+#Trust Center
+$Privacy = Test-Path "HKCU:\Software\Microsoft\Office\15.0\Common\Privacy"
+If ($Privacy -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\15.0\Common" -Name "Privacy"
+}
+
+$Anonymous = Test-Path -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Privacy\SettingsStore\Anonymous"
+
+If ($Anonymous -eq $true){
+  Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Privacy\SettingsStore\Anonymous" -Name "ControllerConnectedServicesState" -value 2
+  $Date = Get-Date -Format 'yyy\-MM\-ddTHH:mm:ssZ'
+  Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Privacy\SettingsStore\Anonymous" -Name "ControllerConnectedServicesStateTime" -value $Date
+}
+
+If ($Anonymous -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Privacy" -Name "SettingsStore"
+  New-Item -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Privacy\SettingsStore" -Name "Anonymous"
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Privacy\SettingsStore\Anonymous" -Name "ControllerConnectedServicesState" -value 2
+  $Date = Get-Date -Format 'yyy\-MM\-ddTHH:mm:ssZ'
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Privacy\SettingsStore\Anonymous" -Name "ControllerConnectedServicesStateTime" -value $Date
+}
+
+
+$Profile1 = Test-Path -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Profiles\$Profile\c02ebc5353d9cd11975200aa004ae40e"
+
+If ($Profile1 -eq $true){
+  Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Profiles\$Profile\c02ebc5353d9cd11975200aa004ae40e" -Name "00030354" -value ([byte[]](0x00,0x00,0x00,0x00))
+}
+
+If ($Profile1 -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Profiles\$Profile" -Name "c02ebc5353d9cd11975200aa004ae40e"
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Profiles\$Profile\c02ebc5353d9cd11975200aa004ae40e" -Name "00030354" -value ([byte[]](0x00,0x00,0x00,0x00))
+}
+
+$Security = Test-Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Security"
+If ($Security -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook" -Name "Security"
+}
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Security" -Name "InitEncrypt" -value 2
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Security" -Name "InitSign" -value 2
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Security" -Name "SharedFolderScript" -value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Security" -Name "PublicFolderScript" -value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Security" -Name "Level" -value 1
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Preferences" -Name "DisableAttachmentPreviewing" -value 1
+
+$Mail = Test-Path -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail"
+If ($Mail -eq $true) {
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail" -Name "UnblockSafeZone" -value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail" -Name "UnblockSpecificSenders" -value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail" -Name "UnblockRSS" -value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail" -Name "UnblockSTS" -value 0
+}
+
+
+If ($Mail -eq $false) {
+  New-Item -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options" -Name "Mail"
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail" -Name "UnblockSafeZone" -value 0
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail" -Name "UnblockSpecificSenders" -value 0
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail" -Name "UnblockRSS" -value 0
+  New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Outlook\Options\Mail" -Name "UnblockSTS" -value 0
+  }
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common" -Name "SendCustomerDataOptInReason" -value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common" -Name "SendCustomerDataOptIn" -value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common" -Name "SendCustomerData" -value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common" -Name "UpdateReliabilityData" -value 0
+
+$Helperviewer = Test-Path "HKCU:\Software\Microsoft\Office\15.0\Common\HelpViewer"
+If ($Helperviewer -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\15.0\Common" -Name "HelpViewer"
+}
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common\HelpViewer" -Name "UseOnlineContent" -value 2
+
+$PTWatson = Test-Path "HKCU:\Software\Microsoft\Office\15.0\Common\PTWatson"
+If ($PTWatson -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\15.0\Common" -Name "PTWatson"
+}
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common\PTWatson" -Name "PTWOptIn" -value 3
+
+$ResearchOptions = Test-Path "HKCU:\Software\Microsoft\Office\15.0\Common\Research\Options"
+If ($ResearchOptions -eq $false){
+  New-Item -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Research" -Name "Options"
+}
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\15.0\Common\Research\Options" -Name "DiscoveryNeedOptIn" -value 0
+
+$Security = Test-Path -Path "HKLM:SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Wow6432Node\Microsoft\Office\15.0\Outlook\Security"
+
+If ($Security -eq $true){
+  Set-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Wow6432Node\Microsoft\Office\15.0\Outlook\Security" -Name "ObjectModelGuard" -value 2
+}
+
+If ($Security -eq $false){
+  New-Item -Path "HKLM:SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Wow6432Node\Microsoft\Office\15.0\Outlook" -Name "Security"
+  New-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Wow6432Node\Microsoft\Office\15.0\Outlook\Security" -Name "ObjectModelGuard" -value 2
+}
+
+
+#Outlook Social Connector
+Write-Host "Disabling Outlook Social Connector"
+Start-Process Powershell .\OSC_Disable.bat
+
+Write-Host "Done"
+Pause
+} 
+
+#######Outlook Not Supported#############
+
+If ($Version -eq "Outlook.Application.14") { 
+  Write-Host "This version of Outlook is not supported"
+}
+
 
 Write-Host "ttyl"
 Get-PSSession | Remove-PSSession
---
+++ Outlook/Outlook_Tools_Menu.ps1
@@ -0,0 +1,105 @@
+<#
+.NAME
+    Outlook Tools
+#>
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$OutlookTools_Menu               = New-Object system.Windows.Forms.Form
+$OutlookTools_Menu.ClientSize    = New-Object System.Drawing.Point(247,300)
+$OutlookTools_Menu.text          = "Outlook Tools"
+$OutlookTools_Menu.TopMost       = $false
+$OutlookTools_Menu.BackColor     = [System.Drawing.ColorTranslator]::FromHtml("#ffffff")
+
+$Rearm_Office                    = New-Object system.Windows.Forms.Label
+$Rearm_Office.text               = "rearm Office"
+$Rearm_Office.AutoSize           = $true
+$Rearm_Office.width              = 150
+$Rearm_Office.height             = 10
+$Rearm_Office.location           = New-Object System.Drawing.Point(14,165)
+$Rearm_Office.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',11)
+
+$TT                              = New-Object system.Windows.Forms.ToolTip
+$TT.ToolTipTitle                 = "Tool_Tip"
+
+$Outlook_Install32               = New-Object system.Windows.Forms.Label
+$Outlook_Install32.text          = "Install Outlook 365 x86"
+$Outlook_Install32.AutoSize      = $true
+$Outlook_Install32.width         = 150
+$Outlook_Install32.height        = 10
+$Outlook_Install32.location      = New-Object System.Drawing.Point(15,15)
+$Outlook_Install32.Font          = New-Object System.Drawing.Font('Microsoft Sans Serif',11)
+
+$Install_Outlook64               = New-Object system.Windows.Forms.Label
+$Install_Outlook64.text          = "Install Outlook 365 x64"
+$Install_Outlook64.AutoSize      = $true
+$Install_Outlook64.width         = 150
+$Install_Outlook64.height        = 10
+$Install_Outlook64.location      = New-Object System.Drawing.Point(15,45)
+$Install_Outlook64.Font          = New-Object System.Drawing.Font('Microsoft Sans Serif',11)
+
+$Outlook_Updates                 = New-Object system.Windows.Forms.Label
+$Outlook_Updates.text            = "Check for Outlook Updates"
+$Outlook_Updates.AutoSize        = $true
+$Outlook_Updates.width           = 150
+$Outlook_Updates.height          = 10
+$Outlook_Updates.location        = New-Object System.Drawing.Point(15,75)
+$Outlook_Updates.Font            = New-Object System.Drawing.Font('Microsoft Sans Serif',11)
+
+$Disable_OSC                     = New-Object system.Windows.Forms.Label
+$Disable_OSC.text                = "Disable Outlook Social Connector"
+$Disable_OSC.AutoSize            = $true
+$Disable_OSC.width               = 150
+$Disable_OSC.height              = 10
+$Disable_OSC.location            = New-Object System.Drawing.Point(15,135)
+$Disable_OSC.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',11)
+
+$Outlook_Profile_Set             = New-Object system.Windows.Forms.Label
+$Outlook_Profile_Set.text        = "Setup Outlook Profile"
+$Outlook_Profile_Set.AutoSize    = $true
+$Outlook_Profile_Set.width       = 150
+$Outlook_Profile_Set.height      = 10
+$Outlook_Profile_Set.location    = New-Object System.Drawing.Point(15,105)
+$Outlook_Profile_Set.Font        = New-Object System.Drawing.Font('Microsoft Sans Serif',11)
+
+$Bypass_O365                     = New-Object system.Windows.Forms.Label
+$Bypass_O365.text                = "Bypass Office 365 (Hybrid)"
+$Bypass_O365.AutoSize            = $true
+$Bypass_O365.width               = 150
+$Bypass_O365.height              = 10
+$Bypass_O365.location            = New-Object System.Drawing.Point(15,195)
+$Bypass_O365.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',11)
+
+$DidItBetter_logo                = New-Object system.Windows.Forms.PictureBox
+$DidItBetter_logo.width          = 94
+$DidItBetter_logo.height         = 45
+$DidItBetter_logo.location       = New-Object System.Drawing.Point(16,235)
+$DidItBetter_logo.imageLocation  = "./Diditbetter_logo.png"
+$DidItBetter_logo.SizeMode       = [System.Windows.Forms.PictureBoxSizeMode]::zoom
+
+$TT.SetToolTip($Rearm_Office,'Re-arms Office Suit Trial Extension')
+$TT.SetToolTip($Outlook_Install32,'Installs Outlook 365 32bit')
+$TT.SetToolTip($Install_Outlook64,'Installs Outlook 365 64bit')
+$TT.SetToolTip($Outlook_Updates,'Checks and runs Outlook 365 Updates')
+$TT.SetToolTip($Disable_OSC,'Disable Outlook Social Connector Add-In')
+$TT.SetToolTip($Outlook_Profile_Set,'Sets up the Outlook Profile for Add2Exchange')
+$TT.SetToolTip($Bypass_O365,'Bypasses O365 Login for Hybrid Environments')
+$OutlookTools_Menu.controls.AddRange(@($Rearm_Office,$Outlook_Install32,$Install_Outlook64,$Outlook_Updates,$Disable_OSC,$Outlook_Profile_Set,$Bypass_O365,$DidItBetter_logo))
+
+
+$Rearm_Office.Add_Click({Start-Process Powershell .\REARM_Office.ps1})
+$Outlook_Install32.Add_Click({Start-Process Powershell .\Outlook_Installer.ps1})
+$Install_Outlook64.Add_Click({Start-Process Powershell .\Outlook_Installer.ps1})
+$Outlook_Updates.Add_Click({Start-Process Powershell .\Office_Updater.ps1})
+$Disable_OSC.Add_Click({Start-Process Powershell .\OSC_Disable.bat})
+$Outlook_Profile_Set.Add_Click({Start-Process Powershell .\Outlook_Profile_Set.ps1})
+$Bypass_O365.Add_Click({Start-Process Powershell .\Bypass AutoDiscover.ps1})
+
+
+
+#Write your logic code here
+
+[void]$OutlookTools_Menu.ShowDialog()
+
+# End Scripting
