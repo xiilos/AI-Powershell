4d1f10407ce9ebaab12d0491f6ca04476383d0b0
xiilos <kouretas.k@gmail.com>
2020-11-21 09:44:38 -0600

    Formating

---
+++ In Development/Send Email.ps1
@@ -0,0 +1,41 @@
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+#Execution Policy
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+
+
+# Script #
+
+#Send Mail Settings
+$SendEmail = $false                    # = $true if you want to enable send report to e-mail (SMTP send)
+$EmailTo   = 'kouretas.k@gmail.com'              #user@domain.something (for multiple users use "User01 &lt;user01@example.com&gt;" ,"User02 &lt;user02@example.com&gt;" )
+$EmailFrom = 'dkouretas@diditbetter.com'   #matthew@domain 
+$EmailSMTP = 'smtp.ex16.diditbetter.com' #smtp server adress, DNS hostname.
+
+
+
+
+# Send e-mail with reports as attachments
+if ($SendEmail -eq $true) {
+  $EmailSubject = "Backup Email $(get-date -format MM.yyyy)"
+  $EmailBody = "Backup Script $(get-date -format MM.yyyy) (last Month).`nYours sincerely `Matthew - SYSTEM ADMINISTRATOR"
+  Logging "INFO" "Sending e-mail to $EmailTo from $EmailFrom (SMTPServer = $EmailSMTP) "
+  ### the attachment is $log 
+  Send-MailMessage -To $EmailTo -From $EmailFrom -Subject $EmailSubject -Body $EmailBody -SmtpServer $EmailSMTP -attachment $Log 
+
+
+$Log = "C:\zlibrary\TEMP\Logs\Event_Log.evtx"
+
+  Send-MailMessage -To "kouretas.k@gmail.com" -From "dkouretas@diditbetter.com" -Subject "Add2Exchange Logs" -Body "This is just a test for email" -SmtpServer "192.168.0.11" -port 25 -attachment $Log
+
+Write-Host "ttyl"
+Get-PSSession | Remove-PSSession
+Exit
+
+# End Scripting
--- Timed Execution Permissions/2010-2019_All_Permissions.ps1
+++ Timed Execution Permissions/2010-2019_All_Permissions.ps1
@@ -2,40 +2,50 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     # Relaunch as an elevated process:
     Start-Process powershell.exe "-File", ('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
     exit
-}
-
-
-#Execution Policy
-
-Set-ExecutionPolicy -ExecutionPolicy Bypass
-
-# Script #
-
-#Variables
-
-$Exchangename = Get-Content "C:\Program Files (x86)\DidItBetterSoftware\Add2Exchange Creds\Exchange_Server_Name.txt"
-$ServiceAccount = Get-Content "C:\Program Files (x86)\DidItBetterSoftware\Add2Exchange Creds\Sync_Account_Name.txt"
-$Username = Get-Content "C:\Program Files (x86)\DidItBetterSoftware\Add2Exchange Creds\Exchange_Server_Admin.txt"
-$Password = Get-Content "C:\Program Files (x86)\DidItBetterSoftware\Add2Exchange Creds\Exchange_Server_Pass.txt" | convertto-securestring
-
-$Cred = New-Object -typename System.Management.Automation.PSCredential `
-    -Argumentlist $Username, $Password
-
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$Exchangename/PowerShell/ -Authentication Kerberos -Credential $Cred -ErrorAction Inquire
-Import-PSSession $Session -DisableNameChecking
-Set-ADServerSettings -ViewEntireForest $true
-
-#Timed Execution Permissions to All Users
-#Get-Mailbox -Resultsize Unlimited | Add-MailboxPermission -User $ServiceAccount -AccessRights FullAccess -InheritanceType all -AutoMapping:$false -confirm:$false
-#Get-Mailbox -Resultsize Unlimited | Where-Object {$_.WhenCreated -ge ((Get-Date).Adddays(-1))} | Add-MailboxPermission -User $ServiceAccount -AccessRights 'FullAccess' -InheritanceType all -AutoMapping:$false -Confirm:$false
-
-
-Get-Mailbox -Resultsize Unlimited | Add-MailboxPermission -User $ServiceAccount -AccessRights FullAccess -InheritanceType all -AutoMapping:$false -confirm:$false
-Get-Mailbox -Resultsize Unlimited | Where-Object {$_.WhenCreated -ge ((Get-Date).Adddays(-90))} | Add-MailboxPermission -User $ServiceAccount -AccessRights 'FullAccess' -InheritanceType all -AutoMapping:$false -Confirm:$false
-
-
-
-Get-PSSession | Remove-PSSession
-Exit
-
-# End Scripting
+  }
+  
+  
+  #Execution Policy
+  
+  Set-ExecutionPolicy -ExecutionPolicy Bypass
+  
+  #Variables
+  
+  $Exchangename = Get-Content "C:\Program Files (x86)\DidItBetterSoftware\Add2Exchange Creds\Exchange_Server_Name.txt"
+  $ServiceAccount = Get-Content "C:\Program Files (x86)\DidItBetterSoftware\Add2Exchange Creds\Sync_Account_Name.txt"
+  $Username = Get-Content "C:\Program Files (x86)\DidItBetterSoftware\Add2Exchange Creds\Exchange_Server_Admin.txt"
+  $Password = Get-Content "C:\Program Files (x86)\DidItBetterSoftware\Add2Exchange Creds\Exchange_Server_Pass.txt" | convertto-securestring
+  
+  # Script #
+  
+  Try {
+  
+    $Cred = New-Object -typename System.Management.Automation.PSCredential `
+      -Argumentlist $Username, $Password
+  
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$Exchangename/PowerShell/ -Authentication Kerberos -Credential $Cred
+    Import-PSSession $Session -DisableNameChecking
+    Set-ADServerSettings -ViewEntireForest $true
+  
+    #Timed Execution Permissions to All Users
+    #Get-Mailbox -Resultsize Unlimited | Add-MailboxPermission -User $ServiceAccount -AccessRights FullAccess -InheritanceType all -AutoMapping:$false -confirm:$false
+    #Get-Mailbox -Resultsize Unlimited | Where-Object {$_.WhenCreated -ge ((Get-Date).Adddays(-1))} | Add-MailboxPermission -User $ServiceAccount -AccessRights 'FullAccess' -InheritanceType all -AutoMapping:$false -Confirm:$false
+  
+  
+    Get-Mailbox -Resultsize Unlimited | Add-MailboxPermission -User $ServiceAccount -AccessRights FullAccess -InheritanceType all -AutoMapping:$false -confirm:$false
+    Get-Mailbox -Resultsize Unlimited | Where-Object { $_.WhenCreated -ge ((Get-Date).Adddays(-90)) } | Add-MailboxPermission -User $ServiceAccount -AccessRights 'FullAccess' -InheritanceType all -AutoMapping:$false -Confirm:$false
+  }
+  
+  Catch {
+  
+    $_.Exception.Message | Write-EventLog -LogName "Add2Exchange" -Source "Add2Exchange" -EventID 8001 -EntryType FailureAudit -Message "$_.Exception.Message"
+    Get-PSSession | Remove-PSSession
+    Exit
+  }
+  
+    Write-EventLog -LogName "Add2Exchange" -Source "Add2Exchange" -EventID 8000 -EntryType SuccessAudit -Message "Add2Exchange PowerShell Added Permissions to All Users Succesfully."
+  
+  Get-PSSession | Remove-PSSession
+  Exit
+  
+  # End Scripting
