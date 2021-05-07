adf23a3e34228fcf537b34429ba53be3e707b0fe
xiilos <kouretas.k@gmail.com>
2021-05-07 14:06:09 -0500

    Added rename to both socialconnector.dll

--- Outlook/OSC_Disable.bat
+++ Outlook/OSC_Disable.bat
@@ -33,3 +33,21 @@ reg query HKLM\SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Mi
 if %ERRORLEVEL% EQU 0 (
 reg add HKLM\SOFTWARE\Microsoft\Office\ClickToRun\REGISTRY\MACHINE\Software\Microsoft\Office\Outlook\Addins\OscAddin.Connect /t REG_DWORD /v LoadBehavior /d 0 /f
 )
+
+#Renaming .dll to .backup
+
+Rename  "C:\Program Files (x86)\Microsoft Office\root\Office16\SOCIALCONNECTOR.DLL" SOCIALCONNECTOR.Backup
+Rename  "C:\Program Files (x86)\Microsoft Office\root\Office16\SOCIALPROVIDER.DLL" SOCIALPROVIDER.Backup
+
+if %ERRORLEVEL% EQU 1 (
+Rename  "C:\Program Files\Microsoft Office\root\Office16\SOCIALCONNECTOR.DLL" SOCIALCONNECTOR.Backup
+Rename  "C:\Program Files\Microsoft Office\root\Office16\SOCIALPROVIDER.DLL" SOCIALPROVIDER.Backup
+)
+
+Rename  "C:\Program Files (x86)\Microsoft Office\root\Office15\SOCIALCONNECTOR.DLL" SOCIALCONNECTOR.Backup
+Rename  "C:\Program Files (x86)\Microsoft Office\root\Office15\SOCIALPROVIDER.DLL" SOCIALPROVIDER.Backup
+
+if %ERRORLEVEL% EQU 1 (
+Rename  "C:\Program Files\Microsoft Office\root\Office15\SOCIALCONNECTOR.DLL" SOCIALCONNECTOR.Backup
+Rename  "C:\Program Files\Microsoft Office\root\Office15\SOCIALPROVIDER.DLL" SOCIALPROVIDER.Backup
+)
