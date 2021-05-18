356a5182a90f835be0a6407c42f248bdf42e02f1
xiilos <kouretas.k@gmail.com>
2021-05-18 13:40:55 -0500

    Added AZURE AD for photo export

--- Exchange_Tools&Permissions/Export_ADPhoto.ps1
+++ Exchange_Tools&Permissions/Export_ADPhoto.ps1
@@ -24,7 +24,8 @@ Push-Location "C:\Program Files (x86)\DidItBetterSoftware\Support\AD_Photos"
 
 $wshell = New-Object -ComObject Wscript.Shell
     
-$answer = $wshell.Popup("Caution... You Must Run this on a box with Active Directory. If the box you are running this on does not have Active Directory; Click Cancel and the File will be Automatically copied to your Clipboard. Otherwise, Click OK to Continue.", 0, "WARNING!!", 0x1)
+$answer = $wshell.Popup("For On-Premise AD ONLY, You Must Run this on a box with Active Directory. Click Cancel and the File will be Automatically copied to your Clipboard; Paste the file in a box with AD. 
+Otherwise, Click OK to Continue.", 0, "WARNING!!", 0x1)
 if ($answer -eq 2) {
     $Location = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\WOW6432Node\OpenDoor Software®\Add2Exchange" -Name "InstallLocation" -ErrorAction SilentlyContinue
     Push-Location $Location
@@ -37,8 +38,6 @@ if ($answer -eq 2) {
 }
 
 
-Import-Module ActiveDirectory
-
 $Title1 = 'AD Photo Export'
 
 Clear-Host 
@@ -46,8 +45,9 @@ Write-Host "================ $Title1 ================"
 ""
 Write-Host "Please Pick From the Following Below"
 ""
-Write-Host "Press '1' for Exporting All Photos from all Users in AD"
-Write-Host "Press '2' for Exporting Photos from a Specific OU"  
+Write-Host "Press '1' for Exporting All Photos from all Users from Active Directory On Premise"
+Write-Host "Press '2' for Exporting Photos from a Specific OU from Active Directory On Premise"
+Write-Host "Press '3' for Exporting All Photos from all Users from Azure Active Directory Online"
 Write-Host "Press 'Q' to Quit." -ForegroundColor Red
 
 $input1 = Read-Host "Please Make A Selection" 
@@ -56,6 +56,7 @@ switch ($input1) {
     '1' { 
         Clear-Host 
         'You Chose to Export All Photos from all Users in AD'
+        Import-Module ActiveDirectory
         Do {
             $users = Get-ADUser -Filter { EmailAddress -like "*@*" } -Properties thumbnailPhoto, mail
     
@@ -74,6 +75,7 @@ switch ($input1) {
     '2' { 
         Clear-Host 
         'You Chose to Export Photos from a Specific OU'
+        Import-Module ActiveDirectory
         Do {
             Write-Host "The Following Questions will Ask for the Path in which you want to search for AD Photos Example: CN=Users,DC=Diditbetter,DC=Local"
             $CN = Read-Host "Please Type in OU or CN Name"
@@ -94,16 +96,58 @@ switch ($input1) {
 
     }
 
+    '3' { 
+        Clear-Host 
+        'You Chose to Export All Photos from Azure Active Directory Online'
+        Import-Module –Name AzureAD -ErrorAction SilentlyContinue
+        If ($error) {
+            Write-Host "Adding Azure AD module"
+            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+            Set-PSRepository -Name psgallery -InstallationPolicy Trusted
+            Install-Module –Name AzureAD -WarningAction "Inquire"
+        }
+         
+        Else { Write-Host 'Module is installed' }
+        
+        Write-Host "Updating Azure AD Module Please Wait..."
+        Update-Module -Name AzureAD
+        Import-Module –Name AzureAD
+        
+        Write-Host "Log in as a Global Administrator"
+        $Credential = Get-Credential
+        Connect-AzureAD -Credential $Credential
+        
+        
+        $Name = Get-AzureADUser | Where-Object { $_.mail } | Select-Object Mail
+        $Location = "C:\Program Files (x86)\DidItBetterSoftware\Support\AD_Photos"
+        $Users = Get-AzureADUser -All $true
+        
+        foreach ($user in $users) { Get-AzureADUserThumbnailPhoto -ObjectId $user.ObjectId -FilePath $location -FileName $user.mail }
+
+
+    }
+
     'q' {
         Write-Host "Quitting"
         Get-PSSession | Remove-PSSession
         Exit  
     } 
 }
+
 Start-Sleep -s 2
 Invoke-Item "C:\Program Files (x86)\DidItBetterSoftware\Support\AD_Photos"
 
+
+$wshell = New-Object -ComObject Wscript.Shell
+    
+$answer = $wshell.Popup("Finished Exporting Azure AD Photos", 0, "Complete", 0x1)
+if ($answer -eq 2) {
+    Break
+}
+
+
 Write-Host "ttyl"
+Disconnect-AzureAD
 Get-PSSession | Remove-PSSession
 Exit
 
