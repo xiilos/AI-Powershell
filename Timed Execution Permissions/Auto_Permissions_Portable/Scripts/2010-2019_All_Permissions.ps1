5086696ba1991eddf0bab39c90f395acc146b570
kouretas.k@gmail.com <kouretas.k@gmail.com>
2022-09-15 09:59:55 -0400

    Moved Folder

---
+++ Timed Execution Permissions/Auto_Permissions_Portable/Scripts/2010-2019_All_Permissions.ps1
@@ -0,0 +1,49 @@
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    # Relaunch as an elevated process:
+    Start-Process powershell.exe "-File", ('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+    exit
+  }
+  
+  
+  #Execution Policy
+  
+  Set-ExecutionPolicy -ExecutionPolicy Bypass
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+  
+  #Variables
+  cd..
+  $Exchangename = Get-Content ".\Add2Exchange Creds\Exchange_Server_Name.txt"
+  $ServiceAccount = Get-Content ".\Add2Exchange Creds\Sync_Account_Name.txt"
+  $Username = Get-Content ".\Add2Exchange Creds\Exchange_Server_Admin.txt"
+  $Password = Get-Content ".\Add2Exchange Creds\Exchange_Server_Pass.txt" | convertto-securestring
+  
+  # Script #
+  
+  Try {
+  
+    $Cred = New-Object -typename System.Management.Automation.PSCredential `
+      -Argumentlist $Username, $Password
+  
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$Exchangename/PowerShell/ -Authentication Kerberos -Credential $Cred
+    Import-PSSession $Session -DisableNameChecking
+    Set-ADServerSettings -ViewEntireForest $true
+  
+  
+  
+    Get-Mailbox -Resultsize Unlimited | Add-MailboxPermission -User $ServiceAccount -AccessRights FullAccess -InheritanceType all -AutoMapping:$false -confirm:$false
+    Get-Mailbox -Resultsize Unlimited | Where-Object { $_.WhenCreated -ge ((Get-Date).Adddays(-90)) } | Add-MailboxPermission -User $ServiceAccount -AccessRights 'FullAccess' -InheritanceType all -AutoMapping:$false -Confirm:$false
+  }
+  
+  Catch {
+  
+    Write-EventLog -LogName "Add2Exchange" -Source "Add2Exchange" -EventID 10001 -EntryType FailureAudit -Message "$_.Exception.Message"
+    Get-PSSession | Remove-PSSession
+    Exit
+  }
+  
+    Write-EventLog -LogName "Add2Exchange" -Source "Add2Exchange" -EventID 10000 -EntryType SuccessAudit -Message "Add2Exchange PowerShell Added Permissions to All Users On-Premise Succesfully."
+  
+  Get-PSSession | Remove-PSSession
+  Exit
+  
+  # End Scripting
---
+++ Timed Execution Permissions/Auto_Permissions_Portable/Scripts/Office365_All_Permissions.ps1
@@ -0,0 +1,45 @@
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    # Relaunch as an elevated process:
+    Start-Process powershell.exe "-File", ('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+    exit
+}
+
+
+#Execution Policy
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Script #
+
+#Variables
+cd..
+$ServiceAccount = Get-Content ".\Add2Exchange Creds\Sync_Account_Name.txt"
+$Username = Get-Content ".\Add2Exchange Creds\GA_Service_Account_Name.txt"
+$Password = Get-Content ".\Add2Exchange Creds\GA_Admin_Pass.txt" | convertto-securestring
+
+Try {
+
+$Cred = New-Object -typename System.Management.Automation.PSCredential `
+    -Argumentlist $Username, $Password
+
+    Connect-ExchangeOnline -Credential $Cred
+
+#Timed Execution Permissions to All Users
+Get-Mailbox -Resultsize Unlimited | Add-MailboxPermission -User $ServiceAccount -AccessRights FullAccess -InheritanceType all -AutoMapping:$false -confirm:$false
+}
+
+
+Catch {
+  
+    Write-EventLog -LogName "Add2Exchange" -Source "Add2Exchange" -EventID 10001 -EntryType FailureAudit -Message "$_.Exception.Message"
+    Get-PSSession | Remove-PSSession
+    Exit
+  }
+  
+    Write-EventLog -LogName "Add2Exchange" -Source "Add2Exchange" -EventID 10000 -EntryType SuccessAudit -Message "Add2Exchange PowerShell Added Permissions to All Users on Office 365 Succesfully."
+  
+  Get-PSSession | Remove-PSSession
+  Exit
+
+# End Scripting
