86c099eb9f527a6aa44b890e682384beaead5bb3
kouretas.k@gmail.com <kouretas.k@gmail.com>
2023-08-30 13:23:48 -0400

    format

---
+++ In Development/SQL8x_to_SQL19.ps1
@@ -0,0 +1,38 @@
+<#
+        .SYNOPSIS
+        
+
+        .DESCRIPTION
+      
+
+        .NOTES
+        Version:        1.0
+        Author:         DidItBetter Software
+
+    #>
+
+
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+#Execution Policy
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+
+#Logging
+Start-Transcript -Path "C:\Program Files (x86)\DidItBetterSoftware\Support\A2E_PowerShell_log.txt" -Append
+
+# Script #
+
+<#
+Steps:
+1. Stop Add2Exchange services and SQL (Put on disable or manual)
+2. Backup A2E SQL into temp folder
+3. Download SQL Expresss 2019
+4. Install SQL Express 2019 with current arguments
+5. Import old DB
+#>
