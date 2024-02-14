85fde90a0af913dee414f6aa56eeb2879db9d918
kouretas.k@gmail.com <kouretas.k@gmail.com>
2024-02-14 13:17:36 -0500

    format

--- SQL/SQL Upgrade Files/SQL_Management_Studio_Quiet_Install.ps1
+++ SQL/SQL Upgrade Files/SQL_Management_Studio_Quiet_Install.ps1
@@ -6,7 +6,7 @@
       
 
         .NOTES
-        Version:        1.0
+        Version:        1.1
         Author:         DidItBetter Software
 
     #>
@@ -26,18 +26,47 @@ Start-Transcript -Path "C:\Program Files (x86)\DidItBetterSoftware\Support\A2E_P
 
 # Script #
 
+#Test for HTTPS Access
+Write-Host "Testing for HTTPS Connectivity"
+
+try {
+    $wresponse = Invoke-WebRequest -Uri https://s3.amazonaws.com/dl.diditbetter.com -UseBasicParsing
+    if ($wresponse.StatusCode -eq 200) {
+        Write-Output "Connection successful"
+    }
+    else {
+        Write-Output "Connection failed with status code $($wresponse.StatusCode)"
+    }
+}
+catch {
+    $wshell1 = New-Object -ComObject Wscript.Shell
+        $answer1 = $wshell1.Popup("Connection failed with error: $($_.Exception.Message)...Click OK to try a different download method or Cancel to Quit", 0, "SQL Download", 0x1)
+        if ($answer1 -eq 2) { 
+            Write-Host "ttyl"
+            Get-PSSession | Remove-PSSession
+            Exit
+        }
+}
 
 #Download SQL Management Studio
-Write-Host "Downloading SQL Management Studio"
-$URL = "https://s3.amazonaws.com/dl.diditbetter.com/SQL%20Express/SSMS-Setup-ENU.exe"
-$Output = "C:\zlibrary\SQL Upgrade\SSMS-Setup-ENU.exe"
-$Start_Time = Get-Date
+try {
+    Write-Host "Downloading SQL Management Studio"
+    $URL = "https://s3.amazonaws.com/dl.diditbetter.com/SQL%20Express/SSMS-Setup-ENU.exe"
+    $Output = "C:\zlibrary\SQL Upgrade\SSMS-Setup-ENU.exe"
+    $Start_Time = Get-Date
     
     (New-Object System.Net.WebClient).DownloadFile($URL, $Output)
     
-Write-Output "Time taken: $((Get-Date).Subtract($Start_Time).Seconds) second(s)"
+    Write-Output "Time taken: $((Get-Date).Subtract($Start_Time).Seconds) second(s)"
     
-Write-Host "Finished Downloading"
+    Write-Host "Finished Downloading"
+    }
+    catch {
+        Write-Host "Donwloading failed through Powershell, trying another way...." -ForegroundColor Red
+        Start-Process "https://s3.amazonaws.com/dl.diditbetter.com/SQL%20Express/SSMS-Setup-ENU.exe"
+        Write-Host "Ensure to put the downloaded SQL Express installer in the folder C:\zlibrary\SQL Upgrade\* When finished, click Enter to continue"
+        Pause
+    }
 
 # Define the path to the SSMS setup executable
 $ssmsSetupPath = "C:\zlibrary\SQL Upgrade\SSMS-Setup-ENU.exe"
